5bea1d49eb26df7074e1233813bc31cbf4b31cd0
Connor Michalec <connormichalec@gmail.com>
2020-08-27 13:48:13 -0500

    Initial commit

---
+++ FChat/FChat/src/main/java/michalec/connor/FChat/App.java
@@ -0,0 +1,13 @@
+package michalec.connor.FChat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
